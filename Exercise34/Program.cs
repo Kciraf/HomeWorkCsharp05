7ac2398f83e58dbee064d11966f38b7287e1865c
Kciraf <valsinat44a@mail.ru>
2023-03-04 15:00:28 +0600

    Added Exercise 34

--- Exercise34/Program.cs
+++ Exercise34/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] GenArray(int size, int low, int high)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(low, high + 1);
+    }
+    return array;
+}
+
+int CountEvenInArray(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0) result++;              
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+   Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+System.Console.Write("Введите размер проверяемого массива: ");
+int testSize = Convert.ToInt32(Console.ReadLine());
+int[] testArr = GenArray(testSize, 100, 999);
+PrintArray(testArr);
+System.Console.WriteLine("Количество четных элементов в массиве: " + CountEvenInArray(testArr));
